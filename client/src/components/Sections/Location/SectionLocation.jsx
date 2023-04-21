6b4cd1a3cad6c4858c153424d85c0c41972fa9cd
jesussfl <jesussflr@gmail.com>
2023-04-21 15:34:27 -0400

    Map fixed

--- client/src/components/Sections/Location/SectionLocation.jsx
+++ client/src/components/Sections/Location/SectionLocation.jsx
@@ -7,49 +7,51 @@ import "./SectionLocation.css";
 function SectionLocation(props) {
   return (
     <section className="section-location" id="contactUs">
-      <div className="copys-location">
-        <div className="texts">
-          <h2>Contáctanos</h2>
-          <p className="body-large">Estaremos encantados de atenderte.</p>
+      <div className="contactUs-wrapper">
+        <div className="copys-location">
+          <div className="texts">
+            <h2>Contáctanos</h2>
+            <p className="body-large">Estaremos encantados de atenderte.</p>
+          </div>
+          <div className="flex-container">
+            <TextField
+              style={{ flexGrow: 0, flexBasis: "calc(50% - 8px)" }}
+              label="Nombre"
+              placeholder="Primer Nombre"
+            />
+            <TextField
+              style={{ flexGrow: 0, flexBasis: "calc(50% - 8px)" }}
+              label="Apellido"
+              placeholder="Primer Apellido"
+            />
+            <TextField
+              style={{ flexBasis: "100%" }}
+              label="Correo Electrónico"
+              placeholder="Correo Electrónico"
+            />
+            <TextField
+              style={{ flexBasis: "100%" }}
+              label="Tu mensaje"
+              multiline={true}
+            ></TextField>
+            <Button
+              style={{ marginTop: "16px", height: "48px", flexBasis: "100%" }}
+              text="Enviar"
+            ></Button>
+          </div>
+          <NavItem />
+          <p className="callout">{props.data.Ubicacion}</p>
         </div>
-        <div className="flex-container">
-          <TextField
-            style={{ flexGrow: 0, flexBasis: "calc(50% - 8px)" }}
-            label="Nombre"
-            placeholder="Primer Nombre"
-          />
-          <TextField
-            style={{ flexGrow: 0, flexBasis: "calc(50% - 8px)" }}
-            label="Apellido"
-            placeholder="Primer Apellido"
-          />
-          <TextField
-            style={{ flexBasis: "100%" }}
-            label="Correo Electrónico"
-            placeholder="Correo Electrónico"
-          />
-          <TextField
-            style={{ flexBasis: "100%" }}
-            label="Tu mensaje"
-            multiline={true}
-          ></TextField>
-          <Button
-            style={{ marginTop: "16px", height: "48px", flexBasis: "100%" }}
-            text="Enviar"
-          ></Button>
+        <div className="map-wrapper">
+          <iframe
+            style={{ width: "100%", height: "100%", borderRadius: "16px" }}
+            className="gmap_iframe"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Crefinex maracay&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          ></iframe>
         </div>
-        <NavItem />
-        <p className="callout">{props.data.Ubicacion}</p>
-      </div>
-      <div className="map-wrapper">
-        <iframe
-          style={{ width: "100%", height: "100%", borderRadius: "20px" }}
-          className="gmap_iframe"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
-          src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Crefinex maracay&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-        ></iframe>
       </div>
     </section>
   );
