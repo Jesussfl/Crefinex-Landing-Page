67d5ba676960c2000dce6015f342010ce23f7f72
Jesus Lopez <jesussflr@gmail.com>
2023-04-30 21:52:09 -0400

    Change folder name

--- client/src/components/Sections/Header/Header.jsx
+++ client/src/components/Sections/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 
 //Methods
-import { loadSheetData } from "../../../api/googleSheetAPI";
+import { loadSheetData } from "../../../services/googleSheetAPI";
 import { handleScrollTo } from "../../../controllers/scroller";
 
 //Components
@@ -15,113 +15,113 @@ import InViewEffect from "../../Decorations/InViewEffect/InViewEffect";
 import { ArrowCircleRight, Sms } from "iconsax-react";
 
 function Header(props) {
-  return (
-    <header id="header">
-      <Confetti
-        style={{
-          position: "absolute",
-          top: "15.5%",
-          left: "-20px",
-          transform: "rotate(-45deg)",
-          zIndex: "-1",
-        }}
-        color="#5171e2"
-        size="36px"
-      />
-      <Confetti
-        style={{
-          position: "absolute",
-          top: "35%",
-          left: "10px",
-          transform: "rotate(10deg)",
-          zIndex: "-1",
-        }}
-        color="#ffe607"
-        size="36px"
-      />
-      <Confetti
-        style={{
-          position: "absolute",
-          top: "15.5%",
-          right: "-20px",
-          transform: "rotate(20deg)",
-          zIndex: "-1",
-        }}
-        color="#ff4463"
-        size="36px"
-      />
-      <Confetti
-        style={{
-          position: "absolute",
-          top: "32%",
-          right: "10px",
-          transform: "rotate(-30deg)",
-          zIndex: "-1",
-        }}
-        color="#7f3dea"
-        size="36px"
-      />
-      <div className="hero">
-        <InViewEffect>
-          <h1>
-            Mejora el futuro <span className="red">financiero</span> de tus{" "}
-            <span className="blue">hijos</span> con nosotros.
-          </h1>
-        </InViewEffect>
-        <InViewEffect>
-          <p className="body-large">{props.data.Subtitulo}</p>
-        </InViewEffect>
-        <div className="actions-container">
-          {/* <TextField type="text" leftIcon={<Sms variant="Bold" />} /> */}
-          <Button
-            text="Ver Nuestros Cursos"
-            onClick={() => handleScrollTo("courses", 0)}
-            rightIcon={<ArrowCircleRight variant="Bold" />}
-          />
-        </div>
-      </div>
-      <div className="people">
-        {" "}
-        <PeopleRoundedBackground
-          className="people1"
-          style={{
-            backgroundColor: "var(--secondary-color-red)",
-            bottom: "2%",
-            left: "3%",
-          }}
-          src="/images/webp/img3.webp"
-        />
-        <PeopleRoundedBackground
-          className="people2"
-          style={{
-            backgroundColor: "var(--primary-color)",
-            bottom: "-60px",
-            left: "25%",
-          }}
-          src="/images/webp/img1.webp"
-        />
-        <PeopleRoundedBackground
-          className="people3"
-          style={{
-            backgroundColor: "var(--secondary-color-yellow)",
-            bottom: "-60px",
-            right: "25%",
-          }}
-          src="/images/webp/img2.webp"
-        />
-        <PeopleRoundedBackground
-          className="people4"
-          style={{
-            backgroundColor: "var(--secondary-color-blue)",
-            bottom: "2%",
-            right: "3%",
-          }}
-          src="/images/webp/img4.webp"
-        />
-      </div>
-      <div className="people-images"></div>
-    </header>
-  );
+   return (
+      <header id="header">
+         <Confetti
+            style={{
+               position: "absolute",
+               top: "15.5%",
+               left: "-20px",
+               transform: "rotate(-45deg)",
+               zIndex: "-1",
+            }}
+            color="#5171e2"
+            size="36px"
+         />
+         <Confetti
+            style={{
+               position: "absolute",
+               top: "35%",
+               left: "10px",
+               transform: "rotate(10deg)",
+               zIndex: "-1",
+            }}
+            color="#ffe607"
+            size="36px"
+         />
+         <Confetti
+            style={{
+               position: "absolute",
+               top: "15.5%",
+               right: "-20px",
+               transform: "rotate(20deg)",
+               zIndex: "-1",
+            }}
+            color="#ff4463"
+            size="36px"
+         />
+         <Confetti
+            style={{
+               position: "absolute",
+               top: "32%",
+               right: "10px",
+               transform: "rotate(-30deg)",
+               zIndex: "-1",
+            }}
+            color="#7f3dea"
+            size="36px"
+         />
+         <div className="hero">
+            <InViewEffect>
+               <h1>
+                  Mejora el futuro <span className="red">financiero</span> de
+                  tus <span className="blue">hijos</span> con nosotros.
+               </h1>
+            </InViewEffect>
+            <InViewEffect>
+               <p className="body-large">{props.data.Subtitulo}</p>
+            </InViewEffect>
+            <div className="actions-container">
+               {/* <TextField type="text" leftIcon={<Sms variant="Bold" />} /> */}
+               <Button
+                  text="Ver Nuestros Cursos"
+                  onClick={() => handleScrollTo("courses", 0)}
+                  rightIcon={<ArrowCircleRight variant="Bold" />}
+               />
+            </div>
+         </div>
+         <div className="people">
+            {" "}
+            <PeopleRoundedBackground
+               className="people1"
+               style={{
+                  backgroundColor: "var(--secondary-color-red)",
+                  bottom: "2%",
+                  left: "3%",
+               }}
+               src="/images/webp/img3.webp"
+            />
+            <PeopleRoundedBackground
+               className="people2"
+               style={{
+                  backgroundColor: "var(--primary-color)",
+                  bottom: "-60px",
+                  left: "25%",
+               }}
+               src="/images/webp/img1.webp"
+            />
+            <PeopleRoundedBackground
+               className="people3"
+               style={{
+                  backgroundColor: "var(--secondary-color-yellow)",
+                  bottom: "-60px",
+                  right: "25%",
+               }}
+               src="/images/webp/img2.webp"
+            />
+            <PeopleRoundedBackground
+               className="people4"
+               style={{
+                  backgroundColor: "var(--secondary-color-blue)",
+                  bottom: "2%",
+                  right: "3%",
+               }}
+               src="/images/webp/img4.webp"
+            />
+         </div>
+         <div className="people-images"></div>
+      </header>
+   );
 }
 
 export default Header;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,23 +3,23 @@ import React, { useState, useEffect } from "react";
 import "./Home.css";
 
 import {
-    SplashScreen,
-    Navbar,
-    Header,
-    SectionIntroduction,
-    SectionAboutUs,
-    SectionMarquee,
-    SectionCourses,
-    SectionEducation,
-    SectionResources,
-    SectionContactUs,
-    SectionSubscribe,
-    Footer,
-    FloatButton,
+   SplashScreen,
+   Navbar,
+   Header,
+   SectionIntroduction,
+   SectionAboutUs,
+   SectionMarquee,
+   SectionCourses,
+   SectionEducation,
+   SectionResources,
+   SectionContactUs,
+   SectionSubscribe,
+   Footer,
+   FloatButton,
 } from "../../components";
 
 //Methods
-import { loadSheetData } from "../../api/googleSheetAPI";
+import { loadSheetData } from "../../services/googleSheetAPI";
 // import {
 //   headerData,
 //   coursesData,
@@ -28,60 +28,61 @@ import { loadSheetData } from "../../api/googleSheetAPI";
 // } from "../../constants/data";
 
 function Home() {
-    //States;
-    const [headerData, setHeaderData] = useState([]);
-    const [coursesData, setCoursesData] = useState([]);
-    const [resourcesData, setResourcesData] = useState([]);
-    const [locationData, setLocationData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+   //States
+   const [headerData, setHeaderData] = useState([]);
+   const [coursesData, setCoursesData] = useState([]);
+   const [resourcesData, setResourcesData] = useState([]);
+   const [locationData, setLocationData] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
 
-    //Refs
-    useEffect(() => {
-        async function fetchData() {
-            window.gapi.load("client", async () => {
-                const headerData = await loadSheetData("Cabecera", "A:B");
-                const coursesData = await loadSheetData("Cursos", "A:D");
-                const resourcesData = await loadSheetData("Recursos", "A:C");
-                const locationData = await loadSheetData("Ubicacion", "A:B");
-                setHeaderData(headerData);
-                setCoursesData(coursesData);
-                setResourcesData(resourcesData);
-                setLocationData(locationData);
-                setIsLoading(false);
-            });
-        }
+   //Refs
+   useEffect(() => {
+      async function fetchData() {
+         window.gapi.load("client", async () => {
+            const headerData = await loadSheetData("Cabecera", "A:B");
+            const coursesData = await loadSheetData("Cursos", "A:D");
+            const resourcesData = await loadSheetData("Recursos", "A:C");
+            const locationData = await loadSheetData("Ubicacion", "A:B");
+            setHeaderData(headerData);
+            setCoursesData(coursesData);
+            setResourcesData(resourcesData);
+            setLocationData(locationData);
+            setIsLoading(false);
+         });
+      }
 
-        fetchData();
-    }, []);
+      fetchData();
+   }, []);
 
-    //Render
-    if (isLoading) {
-        return <SplashScreen />;
-    }
-    return (
-        <div className="home">
-            <FloatButton />
-            <Navbar />
-            <Header data={headerData} />
-            <SectionIntroduction />
-            <SectionMarquee />
-            <SectionCourses data={coursesData} />
-            <SectionEducation />
-            <SectionAboutUs />
-            <section
-                style={{
-                    height: "456px",
-                    width: "100%",
-                    backgroundImage: "url('/images/webp/books.webp')",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                }}></section>
-            <SectionResources data={resourcesData} />
-            <SectionContactUs data={locationData} />
-            <SectionSubscribe />
-            <Footer />
-        </div>
-    );
+   //Render
+   if (isLoading) {
+      return <SplashScreen />;
+   }
+
+   return (
+      <div className="home">
+         <FloatButton />
+         <Navbar />
+         <Header data={headerData} />
+         <SectionIntroduction />
+         <SectionMarquee />
+         <SectionCourses data={coursesData} />
+         <SectionEducation />
+         <SectionAboutUs />
+         <section
+            style={{
+               height: "456px",
+               width: "100%",
+               backgroundImage: "url('/images/webp/books.webp')",
+               backgroundRepeat: "no-repeat",
+               backgroundPosition: "center",
+            }}></section>
+         <SectionResources data={resourcesData} />
+         <SectionContactUs data={locationData} />
+         <SectionSubscribe />
+         <Footer />
+      </div>
+   );
 }
 
 export default Home;
