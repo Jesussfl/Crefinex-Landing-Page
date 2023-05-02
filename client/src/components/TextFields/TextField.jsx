6793935850bfca2f9f9136019afae85b0a978e57
jesussfl <jesussflr@gmail.com>
2023-05-02 16:58:37 -0400

    Add new page with forms to make the purchase

--- client/src/components/TextFields/TextField.jsx
+++ client/src/components/TextFields/TextField.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./TextField.css";
 export function TextField(props) {
-  const InputComponent = props.multiline ? "textarea" : "input";
-  return (
-    <div className="text-field-wrapper" style={props.style}>
-      <label className="label callout">{props.label}</label>
-      <div className="text-field-container">
-        {props.leftIcon ? <i>{props.leftIcon}</i> : ""}
-        <InputComponent
-          type={props.type}
-          name={props.name}
-          placeholder={props.placeholder}
-          value={props.value}
-          onChange={props.onChange}
-        />
+   const InputComponent = props.multiline ? "textarea" : "input";
+   return (
+      <div className="text-field-wrapper" style={props.style}>
+         <label className="label callout">{props.label}</label>
+         <div className="text-field-container">
+            {props.leftIcon ? <i>{props.leftIcon}</i> : ""}
+            <InputComponent
+               type={props.type}
+               name={props.name}
+               placeholder={props.placeholder}
+               value={props.value}
+               onChange={props.onChange}
+            />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default TextField;
