f74a68237f84c37e77bfa23a59b6a9fbbf2f3ee0
jesussfl <jesussflr@gmail.com>
2023-05-05 15:07:51 -0400

    new path for textfield to fix build problems

--- client/src/components/Sections/Subscribe/SectionSubscribe.jsx
+++ client/src/components/Sections/Subscribe/SectionSubscribe.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import "./SectionSubscribe.css";
 import Button from "../../Buttons/Button";
-import TextField from "../../TextFields/TextField";
+import TextField from "../../Textfields/TextField";
 import { ArrowCircleRight, Sms } from "iconsax-react";
 
 function Subscribe() {
-  return (
-    <div className="section-subscribe">
-      <div className="copys-subscribe">
-        <h2>Mejoremos el futuro financiero de tu hijo.</h2>
-        <p className="headline">Adquiere nuestra guía gratuita</p>
-        <div className="actions">
-          <TextField
-            type="email"
-            placeholder="Correo Electronico"
-            leftIcon={<Sms variant="Bold" />}
-          />
-          <Button
-            text="Obtener"
-            rightIcon={<ArrowCircleRight variant="Bold" />}
-          />
-        </div>
+   return (
+      <div className="section-subscribe">
+         <div className="copys-subscribe">
+            <h2>Mejoremos el futuro financiero de tu hijo.</h2>
+            <p className="headline">Adquiere nuestra guía gratuita</p>
+            <div className="actions">
+               <TextField
+                  type="email"
+                  placeholder="Correo Electronico"
+                  leftIcon={<Sms variant="Bold" />}
+               />
+               <Button
+                  text="Obtener"
+                  rightIcon={<ArrowCircleRight variant="Bold" />}
+               />
+            </div>
+         </div>
+         <div className="image-subscribe">
+            <img src="images/webp/girl-with-table.webp" alt="" />
+         </div>
       </div>
-      <div className="image-subscribe">
-        <img src="images/webp/girl-with-table.webp" alt="" />
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Subscribe;
