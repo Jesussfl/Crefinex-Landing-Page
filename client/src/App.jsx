51beb025717e6a04ad043221ae89026b938cfd28
Jesus Lopez <jesussflr@gmail.com>
2023-05-07 07:10:00 -0400

    Modularize react query code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,34 +9,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Checkout from "./pages/Checkout/Checkout";
-
-import { QueryClientProvider, QueryClient } from "react-query";
-import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
-import { persistQueryClient } from "react-query/persistQueryClient-experimental";
-
-const MAX_AGE = 24000;
-
-const queryClient = new QueryClient({
-   defaultOptions: {
-      queries: {
-         refetchOnMount: false,
-         refetchOnWindowFocus: false,
-         refetchOnReconnect: false,
-         staleTime: 0,
-         retry: true,
-         cacheTime: MAX_AGE,
-      },
-   },
-});
-
-const localStoragePersistor = createWebStoragePersistor({
-   storage: window.localStorage,
-});
-
-persistQueryClient({
-   queryClient,
-   persistor: localStoragePersistor,
-});
+import { queryClient, QueryClientProvider } from "./constants/queryClient";
 
 function App() {
    return (
