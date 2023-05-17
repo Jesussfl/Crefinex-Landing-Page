f43631134ffbe297199346b45fe504efbd460eb6
jesussfl <jesussflr@gmail.com>
2023-05-17 09:31:50 -0400

    New color

--- client/src/components/Sections/Resources/SectionResources.jsx
+++ client/src/components/Sections/Resources/SectionResources.jsx
@@ -17,12 +17,13 @@ function SectionResources(props) {
          <div
             className="resources__banner"
             style={{
-               height: "456px",
+               height: "556px",
                width: "100%",
                backgroundImage:
-                  "url('https://ik.imagekit.io/jeflr/Crefinex-Landing-Page/Covers/books-background.jpg?updatedAt=1682943003787')",
+                  "url('https://ik.imagekit.io/jeflr/Crefinex-Landing-Page/Covers/Banner-Libros.jpg?updatedAt=1683921217394')",
                backgroundRepeat: "no-repeat",
                backgroundPosition: "center",
+               backgroundSize: "cover",
                display: "flex",
                justifyContent: "center",
                alignItems: "center",
@@ -32,13 +33,12 @@ function SectionResources(props) {
                   color: "white",
                   textShadow: "5px 5px 5px rgba(0, 0, 0, 0.5)",
                }}>
-               ¡Contamos con libros educativos!
+               {/* ¡Contamos con libros educativos! */}
             </h2>
          </div>
          <div className="copys-resources">
             <p className="body-large">
-               Hemos creado una serie de libros y cuentos dirigidos a niños y
-               jóvenes para ayudarles a comprender conceptos financieros
+               Hemos creado una serie de libros y cuentos dirigidos a niños y jóvenes para ayudarles a comprender conceptos financieros
                importantes de manera sencilla y divertida.
             </p>
          </div>
